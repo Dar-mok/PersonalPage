a0f16c0b44e42dcfab322753231d8cda61a605a3
Russell Jones <justyrones@gmail.com>
2023-10-12 13:47:43 -0600

    reverted the clean.js script to original, removed the upath module from devdependecies

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,6 +1,6 @@
 const sh = require('shelljs');
-// const upath = require('upath');
-const upath = require('./node_modules/upath');
+const upath = require('upath');
+// const upath = require('./node_modules/upath');
 
 
 const destPath = upath.resolve(upath.dirname(__filename), '../dist');
