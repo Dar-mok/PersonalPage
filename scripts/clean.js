12d6ba205b69b7935654bc62f5a82aae7d32c969
Russell Jones <justyrones@gmail.com>
2023-10-12 13:37:03 -0600

    making a new rewuire path for the upath dependency being required

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,5 +1,7 @@
 const sh = require('shelljs');
-const upath = require('upath');
+// const upath = require('upath');
+const upath = require('./node_modules/upath');
+
 
 const destPath = upath.resolve(upath.dirname(__filename), '../dist');
 
